577fb32cd7e00fa293f6c53e456a136923b5447a
Martin Colello <martin@colello.org>
2018-11-09 11:35:01 -0700

    Added codec to end of output for each file

--- find_non_h264.pl
+++ find_non_h264.pl
@@ -16,12 +16,17 @@ foreach(@files){
   my $file = $_;
   if ( $file =~ /\.sub$/ ) { next }
   if ( $file =~ /\.srt$/ ) { next }
+  if ( $file =~ /\.nfo$/ ) { next }
+  if ( $file =~ /\.txt$/ ) { next }
   if ( -f $file ) {
     my $output = `ffprobe "$file" 2>&1`;
     if ( $output =~ /Duration: N\/A/ ) { next }
     if ( $output =~ /Unable to open/ ) { next }
     if ( $output !~ /h264/ ) {
-      print "$file\n";
+      my $output2 = `ffprobe -show_streams "$file" 2>&1`;
+      $output2 =~ /codec_name=(\w+)/;
+      my $codec = $1;
+      print "$file $codec\n";
     }
   }
 }
