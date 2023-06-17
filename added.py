7d35c0fe551c2fed60418641792d280ed23f70a4
Martin Colello <martin@colello.org>
2023-06-17 16:07:36 -0700

    Add files via upload
    Remove movies from recently added list

---
+++ added.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os 
+import sys
+import plexapi
+
+from plexapi.server import PlexServer
+baseurl = 'http://192.168.2.5:32400'
+token = '<YOUR PLEX TOKEN>'
+plex = PlexServer(baseurl, token)
+library = plex.library.section("Movies")
+video = library.get(title="<MOVIE NAME")
+updates = {"addedAt.value": "2018-08-21 11:19:43"}
+
+video.edit(**updates)
